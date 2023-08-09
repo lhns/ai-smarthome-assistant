6ef367c75994de710a1c0bd7e355dfb2dbbca20c
Pierre Kisters <pierrekisters@gmail.com>
2023-08-09 16:18:36 +0200

    smarthome test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import cats.effect._
+import cats.syntax.option._
 import com.github.markusbernhardt.proxy.ProxySearch
 import org.http4s.client.Client
 import org.http4s.jdkhttpclient.JdkHttpClient
@@ -13,19 +14,10 @@ import sttp.openai.requests.completions.chat.ChatRequestResponseData.ChatRespons
 import sttp.openai.requests.completions.chat.{Message, Role}
 
 import java.net.ProxySelector
+import scala.sys.env
 import scala.util.chaining._
 
 object Main extends IOApp {
-  val chatRequestBody: ChatBody = ChatBody(
-    model = ChatCompletionModel.CustomChatCompletionModel("vicuna-7b-v1.3"),
-    messages = Seq(
-      Message(
-        role = Role.User,
-        content = "Hello!"
-      )
-    )
-  )
-
   def createChatCompletion(baseUri: Uri, authToken: String, chatBody: ChatBody): Request[Either[OpenAIException, ChatResponse]] =
     basicRequest.auth
       .bearer(authToken)
@@ -38,6 +30,36 @@ object Main extends IOApp {
       Resource.eval(request.toStrict(None)).flatMap(client.run)
     })
 
+  trait AI {
+    def query(request: String): IO[String]
+  }
+
+  object AI {
+    def apply(backend: Backend[IO], openAiBaseUrl: Uri, openAiApiKey: String): AI = new AI {
+      override def query(request: String): IO[String] =
+        createChatCompletion(openAiBaseUrl, openAiApiKey, ChatBody(
+          model = ChatCompletionModel.CustomChatCompletionModel("vicuna-7b-v1.3"),
+          messages = Seq(
+            Message(
+              role = Role.User,
+              content = request
+            )
+          )
+        ))
+          .send(backend)
+          .map(_.body.toTry.get.choices.headOption.map(_.message.content).orEmpty)
+    }
+  }
+
+  val jsonExample: String =
+    """{
+      |  "device": "<device name>",
+      |  "state": true/false
+      |}""".stripMargin
+
+  val query =
+    s"You are a smart home assistant. For a query you generate a json that matches the following example:\n$jsonExample\nYou don't explain your response and just output the raw json.\nTurn off the test light."
+
   override def run(args: List[String]): IO[ExitCode] = {
     ProxySelector.setDefault(
       Option(new ProxySearch().tap { s =>
@@ -47,12 +69,14 @@ object Main extends IOApp {
         .getOrElse(ProxySelector.getDefault)
     )
 
+    val openAiBaseUrl: Uri = Uri.unsafeParse(env.getOrElse("OPENAI_BASE_URL", "https://api.openai.com/v1"))
+    val openAiApiKey: String = env.getOrElse("OPENAI_API_KEY", throw new RuntimeException("Missing OPENAI_API_KEY!"))
+
     for {
       client <- JdkHttpClient.simple[IO]
       backend = backendFromClient(client)
-      response <- createChatCompletion(uri"https://example.com/v1", "<api-key>", chatRequestBody)
-        .send(backend)
-        .map(_.body)
+      ai = AI(backend, openAiBaseUrl, openAiApiKey)
+      response <- ai.query(query)
       _ = println(response)
     } yield ExitCode.Success
   }
